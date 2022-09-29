09b0a6a04dbff9f15958c5ecbb76f2c05ef46318
satriaberliano <90611336+satriaberliano@users.noreply.github.com>
2022-09-29 20:18:10 +0700

    Update NotesItemButton.js
    remove archived

--- src/components/NotesItemButton.js
+++ src/components/NotesItemButton.js
@@ -4,7 +4,7 @@ function NotesItemButton({ id, archived, onDelete, onArchive}){
   return(
     <div className="notes-item__button">
       <button className="notes-item__delete-button" onClick={() => onDelete(id)}>Hapus</button>
-      <button className="notes-item__archive-button" archived={archived} onClick={() => onArchive(id)}>{ archived ? "Pindahkan" : "Arsipkan"}</button>
+      <button className="notes-item__archive-button" onClick={() => onArchive(id)}>{ archived ? "Pindahkan" : "Arsipkan"}</button>
     </div>
   )
 }
